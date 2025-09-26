e55bd43cb9f181290ebcba16c9e7738990e883a8
IgorZ <igorzomm3r@gmail.com>
2025-09-26 13:54:28 +0200

    branch name was changed

--- infra/config-server/src/main/java/ru/yandex/practicum/ConfigServer.java
+++ infra/config-server/src/main/java/ru/yandex/practicum/ConfigServer.java
@@ -9,7 +9,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigServer {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ConfigServer.class, args);
+
     }
 
 }
