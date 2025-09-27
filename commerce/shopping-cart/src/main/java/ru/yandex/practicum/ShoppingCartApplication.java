1bdb9cfcec0ab06eb1be023c2cdab275b4171b3d
IgorZ <igorzomm3r@gmail.com>
2025-09-27 12:01:59 +0200

    fix duplicated @EnableFeignClients

--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/ShoppingCartApplication.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/ShoppingCartApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
+@EnableFeignClients(basePackages = "ru.yandex.practicum.commerce.shoppingcart.client")
 public class ShoppingCartApplication {
 
     public static void main(String[] args) {
