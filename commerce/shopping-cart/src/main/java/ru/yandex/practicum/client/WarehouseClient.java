f1bdbab68286597b68e9039b6de23876618399f7
IgorZ <igorzomm3r@gmail.com>
2025-09-29 13:11:58 +0200

    fix empty file

---
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/client/WarehouseClient.java
@@ -0,0 +1,32 @@
+package ru.yandex.practicum.client;
+
+import jakarta.validation.Valid;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import ru.yandex.practicum.dto.AddressDto;
+import ru.yandex.practicum.dto.BookedProductsDto;
+import ru.yandex.practicum.dto.ShoppingCartDto;
+import ru.yandex.practicum.request.AddProductToWarehouseRequest;
+import ru.yandex.practicum.request.NewProductInWarehouseRequest;
+
+@FeignClient(name = "warehouse")
+public interface WarehouseClient {
+
+    @PutMapping("/api/v1/warehouse")
+    void newProductInWarehouse(@RequestBody @Valid NewProductInWarehouseRequest requestDto);
+
+    @PostMapping("/api/v1/warehouse/check")
+    BookedProductsDto checkProductQuantityEnoughForShoppingCart(@RequestBody @Valid ShoppingCartDto shoppingCartDto);
+
+    @PostMapping("/api/v1/warehouse/add")
+    void addProductToWarehouse(@RequestBody @Valid AddProductToWarehouseRequest requestDto);
+
+    @GetMapping("/api/v1/warehouse/address")
+    AddressDto getAddress();
+
+    @PostMapping("/api/v1/warehouse/booking")
+    BookedProductsDto bookingProducts(@RequestBody @Valid ShoppingCartDto shoppingCartDto);
+}
--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/client/WarehouseFeignClient.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum.client;
-
-import org.springframework.cloud.openfeign.FeignClient;
-import ru.yandex.practicum.feign.WarehouseClient;
-
-@FeignClient(name = "warehouse")
-public interface WarehouseFeignClient extends WarehouseClient {
-}
