9ee4c78c796ae0b688ac5772a8681f98cd692b1a
IgorZ <igorzomm3r@gmail.com>
2025-10-01 10:00:47 +0200

    delete 2 useless classes

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/circuitBreaker/WarehouseServerUnavailable.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.interactionapi.circuitBreaker;
-
-public class WarehouseServerUnavailable extends RuntimeException {
-    public WarehouseServerUnavailable(String message) {
-        super(message);
-    }
-}
