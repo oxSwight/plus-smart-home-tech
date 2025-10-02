b3442083cd389a492fc05633bfbfb24ae23c9f70
IgorZ <igorzomm3r@gmail.com>
2025-10-02 13:34:21 +0200

    updating application.yaml

---
+++ commerce/payment/src/main/java/ru/yandex/practicum/PaymentApplication.java
@@ -0,0 +1,19 @@
+package ru.yandex.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import ru.yandex.practicum.feign.OrderClient;
+import ru.yandex.practicum.feign.ShoppingStoreClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients(clients = {ShoppingStoreClient.class, OrderClient.class})
+public class PaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication.class, args);
+    }
+
+}
