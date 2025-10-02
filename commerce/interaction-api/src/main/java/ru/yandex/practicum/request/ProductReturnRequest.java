165a82864ff71a8e8a42e0736d4a601960661a0a
IgorZ <igorzomm3r@gmail.com>
2025-10-02 13:32:22 +0200

    updating request

---
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/request/ProductReturnRequest.java
@@ -0,0 +1,21 @@
+package ru.yandex.practicum.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ProductReturnRequest {
+    @NotNull
+    UUID orderId;
+
+    @NotNull
+    Map<UUID, Long> products;
+}
---
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/request/ShippedToDeliveryRequest.java
@@ -0,0 +1,22 @@
+package ru.yandex.practicum.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ShippedToDeliveryRequest {
+    @NotNull
+    UUID orderId;
+
+    @NotNull
+    UUID deliveryId;
+}
