cc77ec6480ec63f0a4d16d51a8f989df01b0b987
IgorZ <igorzomm3r@gmail.com>
2025-10-06 11:42:10 +0200

    fix log by added error to log

--- commerce/delivery/src/main/java/ru/yandex/practicum/controller/DeliveryController.java
+++ commerce/delivery/src/main/java/ru/yandex/practicum/controller/DeliveryController.java
@@ -24,7 +24,7 @@ public class DeliveryController {
             log.info("Создать новую доставку в БД {}", deliveryDto);
             return deliveryService.planDelivery(deliveryDto);
         } catch (Exception e) {
-            log.error("Ошибка создания новой доставки.");
+            log.error("Ошибка создания новой доставки.", e);
             throw e;
         }
     }
